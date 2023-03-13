3d5e929320b364b17e207dc2ad0bc94cab403dc1
Ilia Nor1 <iliacherepanin@gmail.com>
2023-03-13 15:09:14 +0300

    init

---
+++ online_shop/Pods/Target Support Files/Pods-online_shop/Pods-online_shop-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_online_shop : NSObject
+@end
+@implementation PodsDummy_Pods_online_shop
+@end
---
+++ online_shop/online_shop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  online_shop
+//
+//  Created by Nor1 on 13.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
